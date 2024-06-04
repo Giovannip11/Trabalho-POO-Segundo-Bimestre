b8b4708b899dcc0d6f88d9aa44e5ec1272a9221c
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2024-06-03 22:51:28 -0300

    Add files via upload

--- JogoDesenvolvido/src/personagens/personagem.java
+++ JogoDesenvolvido/src/personagens/personagem.java
@@ -5,42 +5,40 @@ import armas.Arma_IF;
 import armas.Magia;
 
 public abstract class personagem {
-      
     Arma_IF arma;
-    
-  
-    
-    public void desenhar(){
-         
+
+    public void desenhar() {
         InOut.MsgDeAviso("Desenho", "Desenhando");
-        
     }
-    public boolean correr(){
-        InOut.MsgDeAviso("Ação", "Tentando correr...");
-        return Math.random() < 0.5;
+
+    public boolean correr() {
+        if (this instanceof DragaoAlado) {
+            InOut.MsgDeAviso("Ação", "Dragao Alado não pode correr!");
+            return false;
+        } else {
+            InOut.MsgDeAviso("Ação", "Tentando correr...");
+            return Math.random() < 0.5;
+        }
     }
-    
-    public String falar(){
-          InOut.MsgDeAviso("Frase","Frase genérica WoooW" );
-     
-          
+
+    public String falar() {
+        InOut.MsgDeAviso("Frase", "Frase genérica WoooW");
         return null;
-        
-     
     }
-    
-    public void arma(){
+
+    public void arma() {
         arma.usarArma();
     }
 
     public void setArma(Arma_IF arma) {
-       if (arma instanceof Magia && !(this instanceof Mago)) {
-            InOut.MsgDeAviso("Erro","Apenas um Mago pode usar Magia!");
+        if (arma instanceof Magia && !(this instanceof Mago)) {
+            InOut.MsgDeAviso("Erro", "Apenas um Mago pode usar Magia!");
+        } else {
+            this.arma = arma;
         }
-        this.arma = arma;
     }
-    }
-    
+}
+
     
     
     
